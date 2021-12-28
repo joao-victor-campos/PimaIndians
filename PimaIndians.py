7ec4ec4e58dc449f29091cf05d1f99b4d4a04337
joao-victor-campos <mmoraes.victor@gmail.com>
2021-12-27 23:12:27 -0300

    Improving Model by removing hidden missing values

--- PimaIndians.py
+++ PimaIndians.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[41]:
 
 
 #importing libs
@@ -10,46 +10,46 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 
-# In[2]:
+# In[42]:
 
 
 #Creating the dataframe
 df = pd.read_csv("C:/Users/facla/Documents/CursoPython/Cap11/pima-data.csv")
 
 
-# In[3]:
+# In[43]:
 
 
 df.shape
 
 
-# In[4]:
+# In[44]:
 
 
 df.head(5)
 
 
-# In[5]:
+# In[45]:
 
 
 #Checking for null values
 df.isnull().values.any()
 
 
-# In[6]:
+# In[46]:
 
 
 #Adicionar linha vazia para teste
 #df = df.append(pd.Series(), ignore_index=True)
 
 
-# In[7]:
+# In[47]:
 
 
 df.isnull()
 
 
-# In[8]:
+# In[48]:
 
 
 #Remoce linha vazia
@@ -57,51 +57,106 @@ df.isnull()
  #   df = df.dropna()
 
 
-# In[9]:
+# In[67]:
 
 
-df.isnull()
+#verificando missing value 
+print("# Linhas no dataframe {0}".format(len(df))) 
+print("# Linhas missing em glucose_conc {0}".format(len(df.loc[df['glucose_conc'] == 0]))) 
+print("# Linhas missing em diastolic_bp {0}".format(len(df.loc[df['diastolic_bp'] == 0]))) 
+print("# Linhas missing em thickness {0}".format(len(df.loc[df['thickness'] == 0]))) 
+print("# Linhas missing em insulin {0}".format(len(df.loc[df['insulin'] == 0]))) 
+print("# Linhas missing em bmi {0}".format(len(df.loc[df['bmi'] == 0]))) 
+print("# Linhas missing em age {0}".format(len(df.loc[df['age'] == 0]))) 
 
 
-# In[10]:
+# In[50]:
 
 
 pd.crosstab(index=df['diabetes'], columns='count')
 
 
-# In[11]:
+# In[64]:
 
 
 import sklearn as sk
 from sklearn.model_selection import train_test_split
-X = df.values
+#usando todas as variáveis como preditivas ["num_preg",	"glucose_conc",	"diastolic_bp",	"thickness", "insulin",	"bmi", "diab_pred", "age", "skin"]
+
+# diabetes é variável alvo
 Y = df["diabetes"].values
 
 
-# In[25]:
+# In[71]:
 
 
 df_train, df_test, y_train, y_teste = train_test_split(df, Y, test_size=0.25, random_state = 42, shuffle = True)
 
-
-# In[13]:
-
-
-
-
-
-# In[14]:
+#Passando as features
+features = ["num_preg", "glucose_conc", "diastolic_bp", "thickness", "insulin",	"bmi", "diab_pred", "age"]
+X_train = df_train[features]
+X_test = df_test[features]
+X_test.head(5)
 
 
+# In[73]:
+
+
+#Using Impute to substitute missing values 
+from sklearn.impute import SimpleImputer
+
+impute_0_to_mean = SimpleImputer(missing_values = 0, strategy = "mean")
+
+X_train = pd.DataFrame(impute_0_to_mean.fit_transform(X_train))
+X_test = pd.DataFrame(impute_0_to_mean.fit_transform(X_test))
+type(X_test)
+X_test.head(100)
+
+
+# In[59]:
 
 
 
-# In[32]:
 
 
+# In[72]:
 
-df_test.drop('diabetes', inplace=True, axis=1)
-df_train.drop('diabetes', inplace=True, axis=1)
+
+#Primeiro Modelo (RandomForest)
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+from sklearn import metrics
+print("{0:.4f}".format(metrics.accuracy_score(y_teste, predictions)))
+
+
+# In[77]:
+
+
+#output the prediction to a csv
+output = pd.DataFrame({'PersonId': X_test.index, 'diabetes': predictions})
+output.to_csv('submission.csv', index=False)
+
+
+# In[74]:
+
+
+#Segundo modelo (GaussianNB() / naive Bayes)
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+y_pred_gnb = gnb.fit(X_train, y_train).predict(X_test)
+print("{0:.4f}".format(metrics.accuracy_score(y_teste, y_pred_gnb)))
+
+
+# In[75]:
+
+
+#Terceiro modelo (Regressão Logística)
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression(C = 0.7, random_state = 42)
+lr.fit(X_train, y_train)
+lr_prediction = lr.predict(X_test)
+print("{0:.4f}".format(metrics.accuracy_score(y_teste, lr_prediction)))
 
 
 # In[ ]:
@@ -110,54 +165,7 @@ df_train.drop('diabetes', inplace=True, axis=1)
 
 
 
-# In[16]:
-
-
-features = ["num_preg",	"glucose_conc",	"diastolic_bp",	"thickness", "insulin",	"bmi", "diab_pred", "age", "skin"]
-X = pd.get_dummies(df_train[features])
-X_test = pd.get_dummies(df_test[features])
-
-
-# In[36]:
-
-
-#Primeiro Modelo (RandomForest)
-model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
-model.fit(X, y_train)
-predictions = model.predict(X_test)
-
-
-# In[37]:
-
-
-output = pd.DataFrame({'PersonId': df_test.index, 'diabetes': predictions})
-output.to_csv('submission.csv', index=False)
-
-
-# In[38]:
-
-
-from sklearn import metrics
-print("{0:.4f}".format(metrics.accuracy_score(y_teste, predictions)))
-
-
-# In[39]:
-
-
-#Segundo modelo (GaussianNB() / naive Bayes)
-from sklearn.naive_bayes import GaussianNB
-gnb = GaussianNB()
-y_pred_gnb = gnb.fit(df_train, y_train).predict(df_test)
-print("{0:.4f}".format(metrics.accuracy_score(y_teste, y_pred_gnb)))
-
-
-# In[30]:
-
-
-
-
-
-# In[34]:
+# In[ ]:
 
 
 
